6e41634aac14c57db157708d7b2f932e6371018a
Md Khairul Hassan Ratul <115990524+mkhRatul@users.noreply.github.com>
2023-10-13 18:28:36 +0100

    error for currency symbol

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -126,7 +126,7 @@ const Cart = () => {
           <div className="cart-bottom">
             <div className="total">
               <h3>Subtotal:</h3>
-              <h3>${totalPrice}</h3>
+              <h3>€{totalPrice}</h3>
             </div>
             <div className="btn-container">
               <button type="button" className="btn" onClick={handleCheckout}>
