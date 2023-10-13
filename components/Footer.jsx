75bcc4b85c9e069261085d0a484d5403f8234ff6
Md Khairul Hassan Ratul <115990524+mkhRatul@users.noreply.github.com>
2023-10-13 18:34:16 +0100

    FooterUpdate

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,7 @@ import { AiFillInstagram, AiOutlineTwitter } from "react-icons/ai";
 const Footer = () => {
   return (
     <div className="footer-container">
-      <p>2022 JSM Headphones All rights reserverd</p>
+      <p>Demo Product</p>
       <p className="icons">
         <AiFillInstagram />
         <AiOutlineTwitter />
